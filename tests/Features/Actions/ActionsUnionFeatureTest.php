699864d770789e5bc8b160b27b9318bf1195773e
v.kuzmenko <mr.vladkuz@gmail.com>
2021-12-20 00:41:20 +0300

    add tests

--- tests/Features/Actions/ActionsUnionFeatureTest.php
+++ tests/Features/Actions/ActionsUnionFeatureTest.php
@@ -4,12 +4,32 @@ declare(strict_types=1);
 
 namespace Dezer\TinkoffInvestApiClient\Tests\Features\Actions;
 
+use DateTime;
 use Dezer\BaseHttpClient\Contracts\HttpActionInterface;
+use Dezer\TinkoffInvestApiClient\Actions\GetOperationsAction;
+use Dezer\TinkoffInvestApiClient\Actions\GetPortfolioAction;
+use Dezer\TinkoffInvestApiClient\Actions\GetPortfolioCurrenciesAction;
+use Dezer\TinkoffInvestApiClient\Actions\GetUserAccountsAction;
 use Dezer\TinkoffInvestApiClient\Actions\Market\GetBondsAction;
+use Dezer\TinkoffInvestApiClient\Actions\Market\GetCandlesAction;
 use Dezer\TinkoffInvestApiClient\Actions\Market\GetCurrenciesAction;
 use Dezer\TinkoffInvestApiClient\Actions\Market\GetETFsAction;
+use Dezer\TinkoffInvestApiClient\Actions\Market\GetOrderBookAction;
 use Dezer\TinkoffInvestApiClient\Actions\Market\GetStocksAction;
+use Dezer\TinkoffInvestApiClient\Actions\Market\SearchByFIGIAction;
+use Dezer\TinkoffInvestApiClient\Actions\Market\SearchByTickerAction;
+use Dezer\TinkoffInvestApiClient\Actions\Orders\CancelOrderAction;
+use Dezer\TinkoffInvestApiClient\Actions\Orders\CreateLimitOrderAction;
+use Dezer\TinkoffInvestApiClient\Actions\Orders\GetOrdersAction;
+use Dezer\TinkoffInvestApiClient\Actions\Sandbox\ClearPositionsAction;
+use Dezer\TinkoffInvestApiClient\Actions\Sandbox\RegisterAccountAction;
+use Dezer\TinkoffInvestApiClient\Actions\Sandbox\RemoveAccountAction;
+use Dezer\TinkoffInvestApiClient\Actions\Sandbox\SetCurrencyBalanceAction;
+use Dezer\TinkoffInvestApiClient\Actions\Sandbox\SetPositionBalanceAction;
 use Dezer\TinkoffInvestApiClient\Dto\BaseResponse;
+use Dezer\TinkoffInvestApiClient\Dto\Market\CandlesCondition;
+use Dezer\TinkoffInvestApiClient\Dto\Operations\OperationsCondition;
+use Dezer\TinkoffInvestApiClient\Enums\IntervalEnum;
 use Dezer\TinkoffInvestApiClient\Enums\ResponseStatusCodeEnum;
 use Dezer\TinkoffInvestApiClient\Tests\Features\AbstractFeatureTest;
 
@@ -23,6 +43,8 @@ class ActionsUnionFeatureTest extends AbstractFeatureTest
         ?string $conditionClass = null,
         array $conditionData = []
     ): void {
+        $this->markTestIncomplete('Необходимо завершить написание тестов.');
+
         $condition = null;
 
         if ($conditionClass !== null) {
@@ -41,15 +63,15 @@ class ActionsUnionFeatureTest extends AbstractFeatureTest
     public function actionDataProvider(): array
     {
         return [
-//            [
-//                GetUserAccountsAction::class,
-//            ],
-//            [
-//                GetPortfolioAction::class,
-//            ],
-//            [
-//                GetPortfolioCurrenciesAction::class,
-//            ],
+            [
+                GetUserAccountsAction::class,
+            ],
+            [
+                GetPortfolioAction::class,
+            ],
+            [
+                GetPortfolioCurrenciesAction::class,
+            ],
             [
                 GetStocksAction::class,
             ],
@@ -62,24 +84,277 @@ class ActionsUnionFeatureTest extends AbstractFeatureTest
             [
                 GetCurrenciesAction::class,
             ],
-
-//            [
-//                GetOperationsAction::class,
-//                OperationsCondition::class,
-//                [
-//                    'from' => (new \DateTime())->modify('-1 month'),
-//                    'to' => new \DateTime(),
-//                ]
-//            ],
-//            [
-//                GetOperationsAction::class,
-//                OperationsCondition::class,
-//                [
-//                    'from' => (new \DateTime())->modify('-1 month'),
-//                    'to' => new \DateTime(),
-//                    'figi' => self::FIGI
-//                ]
-//            ],
+            // Добавить гуард в экшн для проверки условия.
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 minute'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_1()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 day'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_1()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-2 minutes'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_2()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 day'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_2()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-3 minutes'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_3()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 day'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_3()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-5 minutes'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_5()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 day'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_5()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-10 minutes'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_10()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 day'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_10()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-15 minutes'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_15()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 day'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_15()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-30 minutes'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_30()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 day'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MIN_30()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 hour'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::HOUR()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-7 days'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::HOUR()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 day'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::DAY()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 year'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::DAY()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-7 days'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::WEEK()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-2 years'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::WEEK()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-1 month'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MONTH()
+                ],
+            ],
+            [
+                GetCandlesAction::class,
+                CandlesCondition::class,
+                [
+                    'figi' => self::FIGI,
+                    'from' => (new DateTime())->modify('-10 years'),
+                    'to' => new DateTime(),
+                    'interval' => IntervalEnum::MONTH()
+                ],
+            ],
+            [
+                GetOrderBookAction::class,
+            ],
+            [
+                SearchByFIGIAction::class,
+            ],
+            [
+                SearchByTickerAction::class,
+            ],
+            [
+                ClearPositionsAction::class,
+            ],
+            [
+                RegisterAccountAction::class,
+            ],
+            [
+                RemoveAccountAction::class,
+            ],
+            [
+                SetCurrencyBalanceAction::class,
+            ],
+            [
+                SetPositionBalanceAction::class,
+            ],
+            [
+                CancelOrderAction::class,
+            ],
+            [
+                CreateLimitOrderAction::class,
+            ],
+            [
+                GetOrdersAction::class,
+            ],
+            [
+                GetOperationsAction::class,
+                OperationsCondition::class,
+                [
+                    'from' => (new DateTime())->modify('-1 month'),
+                    'to' => new DateTime(),
+                ]
+            ],
+            [
+                GetOperationsAction::class,
+                OperationsCondition::class,
+                [
+                    'from' => (new DateTime())->modify('-1 month'),
+                    'to' => new DateTime(),
+                    'figi' => self::FIGI
+                ]
+            ],
         ];
     }
 }

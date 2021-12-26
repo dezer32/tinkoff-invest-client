1a58525a78dcc6ad8b52b6fff832d534eb4f5f0a
v.kuzmenko <mr.vladkuz@gmail.com>
2021-12-26 12:55:13 +0300

    Fix client to sdk test on bounds

--- tests/Integration/Action/Market/GetBondsActionIntegrationTest.php
+++ tests/Integration/Action/Market/GetBondsActionIntegrationTest.php
@@ -2,8 +2,6 @@
 
 namespace Dezer\TinkoffInvestApiClient\Tests\Integration\Action\Market;
 
-use Dezer\TinkoffInvestApiClient\Actions\Market\GetBondsAction;
-use Dezer\TinkoffInvestApiClient\Dto\Market\Bonds\InvestmentSecuritiesBondsResponse;
 use Dezer\TinkoffInvestApiClient\Enums\ResponseStatusCodeEnum;
 use Dezer\TinkoffInvestApiClient\Tests\Integration\AbstractIntegrationTest;
 
@@ -11,10 +9,7 @@ class GetBondsActionIntegrationTest extends AbstractIntegrationTest
 {
     public function testSuccessCanGetBonds(): void
     {
-        $action = new GetBondsAction();
-
-        /** @var InvestmentSecuritiesBondsResponse $response */
-        $response = $this->client->perform($action);
+        $response = $this->sdk->getBonds();
 
         self::assertTrue($response->getStatus()->equals(ResponseStatusCodeEnum::OK()));
     }
